98c27cf4f71974a36cfed5c18123f7af9d901ec2
aanimesh23 <animesha@uci.edu>
2020-02-07 11:22:13 -0800

    Initial Commit

---
+++ TreatYoSelf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TreatYoSelf
+//
+//  Created by Animesh Agrawal on 2/7/20.
+//  Copyright © 2020 Animesh Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
