fede07a854e5ee020ff522e2fe620a6e6507c3a4
aanimesh23 <animesha@uci.edu>
2020-03-12 14:18:46 -0700

    Update FoodJournalViewController.swift

--- TreatYoSelf/ViewControllers/FoodJournalViewController.swift
+++ TreatYoSelf/ViewControllers/FoodJournalViewController.swift
@@ -9,21 +9,35 @@
 import UIKit
 import HealthKit
 import Parse
-class FoodJournalViewController: UIViewController {
+class FoodJournalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var journalTable: UITableView!
     
     @IBOutlet weak var calorieBurnt: UILabel!
+    var log = [PFObject]()
+    
     let healthStore = HKHealthStore()
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hey")
+        
+        journalTable.delegate = self
+        journalTable.dataSource = self
         //calorieBurnt.text = try healthStore.activeEnergyBurned()
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        let query = PFQuery(className: "FoodJournalObject")
+        query.limit = 20
         
+        query.findObjectsInBackground { (log, error) in
+        if log != nil {
+            self.log = log!
+            self.journalTable.reloadData()
+            }
+        }
     }
     
     @IBAction func onLogout(_ sender: Any) {
@@ -51,6 +65,15 @@ class FoodJournalViewController: UIViewController {
             print("Error: \(String(describing: error))")
         }
         }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = journalTable.dequeueReusableCell(withIdentifier: "PersonalJournalTableViewCell", for: indexPath) as! PersonalJournalTableViewCell
+        
+        return cell
     }
     /*
     // MARK: - Navigation
